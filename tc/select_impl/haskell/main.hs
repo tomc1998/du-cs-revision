43b3486b1bd976f4637503bcb3eca45abaf6d876
Thomas Cheng <thomascheng1998@googlemail.com>
2017-10-30 12:53:24 +0000

    Replaced partition function
    New function takes a custom pivot to partition by

--- tc/select_impl/haskell/main.hs
+++ tc/select_impl/haskell/main.hs
@@ -1,6 +1,17 @@
 
--- The partition function. This uses the first element of the list as the
--- partition, and moves the list elements to either side of the partition
--- (based on if they're lower / heigher than the chosen pivot element).
-partition :: Ord a => [a] -> [a]
-partition (pivot:xs) = concat [[y | y <- xs, y < pivot], [pivot], [y | y <- xs, y > pivot]]
+-- Partition using a custom pivot. The first parameter is an integer, which is
+-- an index into the list pointing to the pivot to use. If this is out of
+-- bounds of the given list, this function will throw.
+partition_custom_pivot :: Ord a => Int -> [a] -> [a]
+partition_custom_pivot pivot_ix xs = 
+  -- Using list comprehensions concat 2 sublists of elements with the chosen
+  -- pivot in between, where the first sublist is the list of elements in xs less
+  -- than the pivot and the second sublist is the list of elements in xs greater
+  -- than the pivot.
+  concat [
+    [y | y <- xs, y < pivot], 
+    [pivot], 
+    [y | y <- xs, y > pivot]
+  ]
+  where pivot = xs !! pivot_ix
+
